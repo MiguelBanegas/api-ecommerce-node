4d85061a79fe2f21a460aab7a280178bef749b5c
MiguelBanegas <miguelangelbanegas@gmail.com>
2025-11-28 13:28:32 -0300

    fix: cors antes de las rutas

--- src/app.js
+++ src/app.js
@@ -6,16 +6,14 @@ require("dotenv").config();
 const app = express();
 
 // Middleware
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-// Routes
-app.use("/api", require("./routes"));
 app.use(cors({
   origin: ['https://mabcontrol.ar', 'http://localhost:3003']
 }));
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+g
+// Routes
+app.use("/api", require("./routes"));
 // Health check
 app.get("/", (req, res) => {
   res.send("API E-commerce is running");
