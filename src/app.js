e9448f218cb9677708fbdfc05534d5668c950fdc
MiguelBanegas <miguelangelbanegas@gmail.com>
2025-11-30 12:34:37 -0300

    cambio asignacion de puerto

--- src/app.js
+++ src/app.js
@@ -81,7 +81,7 @@ app.post("/notify-build", (req, res) => {
   }
 });
 
-const PORT = 3003;
+const PORT = process.env.PORT || 3003;
 
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
