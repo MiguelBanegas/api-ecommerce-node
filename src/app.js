7ef9a1ff244c67bf30ac484cdb78bc7f09ede42c
MiguelBanegas <miguelangelbanegas@gmail.com>
2025-11-30 14:48:33 -0300

    desactivo la funcion de notificacion por websockets

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const http = require("http");
 const path = require("path");
-const { wss } = require("./ws-server");
+//const { wss } = require("./ws-server");
 const cors = require("cors");
 const morgan = require("morgan"); // Optional logging
 const setupCleanupCron = require("./utils/cleanupCron");
@@ -60,26 +60,26 @@ app.get("/", (req, res) => {
 });
 
 // Endpoint para notificar a clientes WebSocket de nuevo build
-app.post("/notify-build", (req, res) => {
-  const deployTime = new Date().toISOString();
-  if (wss) {
-    // broadcast a todos los clientes conectados
-    wss.clients.forEach((client) => {
-      if (client.readyState === client.OPEN) {
-        client.send(JSON.stringify({ type: "NEW_BUILD", deployTime }));
-      }
-    });
-    res.send({
-      status: "ok",
-      message: "Notificación enviada a los clientes",
-      deployTime,
-    });
-  } else {
-    res
-      .status(500)
-      .send({ status: "error", message: "WebSocket server no disponible" });
-  }
-});
+/* app.post("/notify-build", (req, res) => { */
+/*   const deployTime = new Date().toISOString(); */
+/*   if (wss) { */
+/*     // broadcast a todos los clientes conectados */
+/*     wss.clients.forEach((client) => { */
+/*       if (client.readyState === client.OPEN) { */
+/*         client.send(JSON.stringify({ type: "NEW_BUILD", deployTime })); */
+/*       } */
+/*     }); */
+/*     res.send({ */
+/*       status: "ok", */
+/*       message: "Notificación enviada a los clientes", */
+/*       deployTime, */
+/*     }); */
+/*   } else { */
+/*     res */
+/*       .status(500) */
+/*       .send({ status: "error", message: "WebSocket server no disponible" }); */
+/*   } */
+/* }); */
 
 const PORT = process.env.PORT || 3003;
 
